8348ea7aa0785b4d4feea3ca505f58a7cffb5769
Lukas Jakobsen <109243@RTS-365.dk>
2019-05-28 11:29:18 +0200

    awesome

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,7 +1,33 @@
 module.exports = (app) => {
 
    app.get('/', (req, res, next) => {
-      res.render('home');
+      res.render('home',  {
+         'title': 'Min Forside'
+      });
+   });
+
+   app.get('/categori', (req, res, next) => {
+      res.render('categori',  {
+         'title': 'Kategori'
+      });
+   });
+
+   app.get('/post', (req, res, next) => {
+      res.render('post',  {   
+         'title': 'Article'
+      });
+   });
+
+   app.get('/about', (req, res, next) => {
+      res.render('about',  {
+         'title': 'About us'
+      });
+   });
+
+   app.get('/contact', (req, res, next) => {
+      res.render('contact',  {
+         'title': 'Kontakt'
+      });
    });
 
 };
