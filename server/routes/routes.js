c765282e80a6ff8fb16f5cf207aa4dd2d0281473
Lukas Jakobsen <109243@RTS-365.dk>
2019-06-17 11:01:15 +0200

    formular til data

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,5 +1,16 @@
 const mysql = require('../config/mysql')
 
+
+async function getCategories() {
+   let db = await mysql.connect();
+   let [categories] = await db.execute(`
+      SELECT category_id, category_title 
+      FROM categories
+      ORDER BY category_title ASC`);
+   db.end();
+   return categories;
+}
+
 module.exports = (app) => {
 
    app.get('/database',  async (req,res,next)=>{
@@ -687,8 +698,62 @@ module.exports = (app) => {
    });
 
    //  tilføjes i routes.js filen f.eks. lige under app.get('/contact') endpoint
-   app.post('/contact', (req, res, next) => {
+   app.post('/contact', async (req, res, next) => {
+      let db = await mysql.connect();
+      let result = await db.execute(`
+         INSERT INTO messages 
+            (message_name, message_email, message_subject, message_text, message_date) 
+         VALUES 
+            (?,?,?,?,?)`, [name, email, subject, message, contactDate]);
+      db.end();
+         // indsamling af værdierne og oprettelse af de nødvendige variabler.
+   
+         // affected rows er større end nul, hvis en (eller flere) række(r) blev indsat
+if (result[0].affectedRows > 0) {
+   return_message.push('Tak for din besked, vi vender tilbage hurtigst muligt');
+} else {
+   return_message.push('Din besked blev ikke modtaget.... ');
+}
+
+   
+   
+   let name = req.body.name;
+   let email = req.body.email;
+   let subject = req.body.subject;
+   let message = req.body.message;
+   let contactDate = new Date();
+ 
+   // håndter valideringen, alle fejl pushes til et array så de er samlet ET sted
+   let return_message = [];
+   if (name == undefined || name == '') {
+      return_message.push('Navn mangler');
+   }
+   if (email == undefined || email == '') {
+      return_message.push('Email mangler');
+   }
+   if (subject == undefined || subject == '') {
+      return_message.push('Emne mangler');
+   }
+   if (message == undefined || message == '') {
+      return_message.push('Beskedteksten mangler');
+   }
+
+   if (return_message.length > 0) {
+      // der er mindst 1 information der mangler, returner beskeden som en string.
+      let categories = await getCategories(); // denne forklares lige om lidt!
+ 
+
+      res.render('contact', {
+         'title': 'Kontakt',
+         "formularAdress": adress,
+         'categories': categories,
+         'return_message': return_message.join(', '),
+         'values': req.body // læg mærke til vi "bare" sender req.body tilbage
+      });
+   
+   } else {
       res.send(req.body);
+   }
 });
 
 };
