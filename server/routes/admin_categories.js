9c52ea185267389e8385b721df4c1b0b3b78ff2d
Lukas Jakobsen <109243@RTS-365.dk>
2019-06-26 13:31:31 +0200

    categories crud

--- server/routes/admin_categories.js
+++ server/routes/admin_categories.js
@@ -1,21 +1,68 @@
 const mysql = require("../config/mysql");
 
+async function getCategories() {
+    let db = await mysql.connect();
+    let [categories] = await db.execute(`
+       SELECT category_id, category_title 
+       FROM categories
+ `);
+    db.end();
+    return categories;
+ }
+
+
 module.exports = app => {
 
    // her placeres alle de routes administrations panelet har brug for 
 
    app.get("/admin/categories", async (req, res, next) => {
     // her skal alle kategorier hentes og sendes til template filen.....
+    let categories = await getCategories();
+    res.render("admin_categories", {
+    "categories": categories
+    })
   });
 
   app.post("/admin/categories", async (req, res, next) => {
     // her skal vi modtage form data og indsætte det i databasen
     // send bruger tilbage til kategori admin listen
+    let title = req.body.category_title;
+
+    let return_message = [];
+    if (title == undefined || title == '') {
+       return_message.push('Navn mangler');
+    }
+
+    if (return_message.length > 0) {
+        // der er mindst 1 information der mangler, returner beskeden som en string.
+        let categories = await getCategories(); // denne forklares lige om lidt!
+
+
+        res.render('admin_categories', {
+           'categories': categories,
+           'return_message': return_message.join(', '),
+           'category': req.body // læg mærke til vi "bare" sender req.body tilbage
+        });
+
+     }
+     else{
+         let db = await mysql.connect();
+              let result = await db.execute(`
+              INSERT INTO categories
+              SET category_title = ?
+             `, [title]);
+              db.end();
+         res.redirect("/admin/categories")
+     }
+
+
   });
 
   app.get("/admin/categories/edit/:category_id", async (req, res, next) => {
     // denne route skal hente både alle kategorier og den ene kategori
     // data skal sendes til template filen
+
+    
   });
 
   app.post("/admin/categories/edit/:category_id", async (req, res, next) => {
@@ -24,10 +71,11 @@ module.exports = app => {
 
     
     let [result] = await db.execute(
-        `
+        `   
             UPDATE categories 
             SET category_title = ?
-            WHERE category_id = ?`,
+            WHERE category_id = ?
+        `,
         [category_title, category_id]
       );
   });
