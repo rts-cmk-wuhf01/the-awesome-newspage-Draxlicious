fd8105c22a0404d0622339162df45c72c94531e7
Lukas Jakobsen <109243@RTS-365.dk>
2019-06-26 15:03:55 +0200

    CRUD finnish

--- server/routes/admin_categories.js
+++ server/routes/admin_categories.js
@@ -8,82 +8,131 @@ async function getCategories() {
  `);
     db.end();
     return categories;
- }
+}
 
 
 module.exports = app => {
 
-   // her placeres alle de routes administrations panelet har brug for 
+    // her placeres alle de routes administrations panelet har brug for 
 
-   app.get("/admin/categories", async (req, res, next) => {
-    // her skal alle kategorier hentes og sendes til template filen.....
-    let categories = await getCategories();
-    res.render("admin_categories", {
-    "categories": categories
-    })
-  });
+    app.get("/admin/categories", async (req, res, next) => {
+        // her skal alle kategorier hentes og sendes til template filen.....
+        let categories = await getCategories();
+        res.render("admin_categories", {
+            "categories": categories
+        })
+    });
 
-  app.post("/admin/categories", async (req, res, next) => {
-    // her skal vi modtage form data og indsætte det i databasen
-    // send bruger tilbage til kategori admin listen
-    let title = req.body.category_title;
+    app.post("/admin/categories", async (req, res, next) => {
+        // her skal vi modtage form data og indsætte det i databasen
+        // send bruger tilbage til kategori admin listen
+        let title = req.body.category_title;
 
-    let return_message = [];
-    if (title == undefined || title == '') {
-       return_message.push('Navn mangler');
-    }
+        let return_message = [];
+        if (title == undefined || title == '') {
+            return_message.push('Navn mangler');
+        }
 
-    if (return_message.length > 0) {
-        // der er mindst 1 information der mangler, returner beskeden som en string.
-        let categories = await getCategories(); // denne forklares lige om lidt!
+        if (return_message.length > 0) {
+            // der er mindst 1 information der mangler, returner beskeden som en string.
+            let categories = await getCategories(); // denne forklares lige om lidt!
 
 
-        res.render('admin_categories', {
-           'categories': categories,
-           'return_message': return_message.join(', '),
-           'category': req.body // læg mærke til vi "bare" sender req.body tilbage
-        });
+            res.render('admin_categories', {
+                'categories': categories,
+                'return_message': return_message.join(', '),
+                'category': req.body // læg mærke til vi "bare" sender req.body tilbage
+            });
 
-     }
-     else{
-         let db = await mysql.connect();
-              let result = await db.execute(`
+        }
+        else {
+            let db = await mysql.connect();
+            let result = await db.execute(`
               INSERT INTO categories
               SET category_title = ?
              `, [title]);
-              db.end();
-         res.redirect("/admin/categories")
-     }
+            db.end();
+            res.redirect("/admin/categories")
+        }
 
 
-  });
+    });
 
-  app.get("/admin/categories/edit/:category_id", async (req, res, next) => {
-    // denne route skal hente både alle kategorier og den ene kategori
-    // data skal sendes til template filen
+    app.get("/admin/categories/edit/:category_id", async (req, res, next) => {
+        // denne route skal hente både alle kategorier og den ene kategori
+        // data skal sendes til template filen
 
-    
-  });
+        let db = await mysql.connect();
+        let [category] = await db.execute(`
+      SELECT category_title
+      FROM categories
+      WHERE category_id = ?
+      `, [req.params.category_id]);
+        db.end();
 
-  app.post("/admin/categories/edit/:category_id", async (req, res, next) => {
-    // tag form data og parameter fra endpoint og opdater databasen
-    // send bruger tilbage til kategori admin listen
+        let categories = await getCategories();
 
-    
-    let [result] = await db.execute(
+        res.render("admin_categories", {
+            "categories": categories,
+            "category": category[0]
+        })
+    });
+
+    app.post("/admin/categories/edit/:category_id", async (req, res, next) => {
+        // tag form data og parameter fra endpoint og opdater databasen
+        // send bruger tilbage til kategori admin listen
+
+
+
+
+
+        let title = req.body.category_title;
+
+        let return_message = [];
+        if (title == undefined || title == '') {
+            return_message.push('Navn mangler');
+        }
+
+        if (return_message.length > 0) {
+            // der er mindst 1 information der mangler, returner beskeden som en string.
+            let categories = await getCategories(); // denne forklares lige om lidt!
+
+
+            res.render('admin_categories', {
+                'categories': categories,
+                'return_message': return_message.join(', '),
+                'category': req.body, // læg mærke til vi "bare" sender req.body tilbage
+            });
+
+        }
+        else {
+            let db = await mysql.connect();
+            let [result] = await db.execute(
         `   
             UPDATE categories 
             SET category_title = ?
             WHERE category_id = ?
         `,
-        [category_title, category_id]
-      );
-  });
+                [title, req.params.category_id]
+            );
+            db.end();
+            res.redirect("/admin/categories")
+        }
 
-  app.get("/admin/categories/delete/:category_id", async (req, res, next) => {
-    // benyt endpoint parameter til at slette en kategori fra databasen
-    // send bruger tilbage til kategori admin listen
+    });
 
-    let [result] = await db.execute('DELETE FROM categories WHERE category_id = ?', [category_id]);
-  });
+
+    app.get("/admin/categories/delete/:category_id", async (req, res, next) => {
+        // benyt endpoint parameter til at slette en kategori fra databasen
+        // send bruger tilbage til kategori admin listen
+
+        let db = await mysql.connect();
+        let [result] = await db.execute(`
+        DELETE FROM categories
+         WHERE category_id = ?`
+         , [req.params.category_id]
+        );
+        db.end();
+        res.redirect("/admin/categories")
+    });
 };
